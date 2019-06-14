8b9b4e0d196454f7b871ab288f995a8988ebef68
WangLaban <42727870+WangLaban@users.noreply.github.com>
2019-06-14 14:09:12 +0800

    Update ex9_24.cpp

--- ch09/ex9_24.cpp
+++ ch09/ex9_24.cpp
@@ -11,8 +11,7 @@
 int main()
 {
     std::vector<int> v;
-    std::cout << v.at(
-        0); // terminating with uncaught exception of type std::out_of_range
+    std::cout << v.at(0);    // terminating with uncaught exception of type std::out_of_range
     std::cout << v[0];       // Segmentation fault: 11
     std::cout << v.front();  // Segmentation fault: 11
     std::cout << *v.begin(); // Segmentation fault: 11
