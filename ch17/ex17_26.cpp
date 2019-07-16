d4857e633bc79826d8869dc04330f1f3fecb88d1
WangLaban <42727870+WangLaban@users.noreply.github.com>
2019-07-16 19:44:54 +0800

    Create ex17_26.cpp

---
+++ ch17/ex17_26.cpp
@@ -0,0 +1,37 @@
+//exercise 17.26
+//Rewrite your phone program so that it writes only the second and subsequent phone numbers for people with more than one phone number.
+
+#include <iostream>
+#include <regex>
+#include <string>
+#include <sstream>
+
+using namespace std;
+
+string pattern = "(\\()?(\\d{3})(\\))?([-. ])?(\\d{3})([-. ])?(\\d{4})";
+string fmt = "$2.$5.$7";
+regex r(pattern);
+string s;
+
+int main()
+{
+    while(getline(cin,s))
+    {
+        unsigned int i = 0;
+        istringstream iss(regex_replace(s, r, fmt));
+        string text;
+        while(iss >> text)
+        {
+        	++i;
+        	if(i != 2)
+        	{
+        		cout<<text<<" ";
+			}
+		}
+		if(i == 2)
+		    cout<<text;
+		cout<<endl;
+    }
+
+    return 0;
+}
