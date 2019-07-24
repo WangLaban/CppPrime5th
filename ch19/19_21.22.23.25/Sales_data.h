985a4e6633f1323907577d70458633bb3cd95772
WangLaban <42727870+WangLaban@users.noreply.github.com>
2019-07-24 18:48:48 +0800

    Create Sales_data.h

---
+++ ch19/19_21.22.23.25/Sales_data.h
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <string>
+class Sales_data {
+	friend std::ostream &operator<<(std::ostream&, const Sales_data&);
+	friend std::istream &operator>>(std::istream&, Sales_data&);
+	friend Sales_data operator+(const Sales_data&, const Sales_data&);
+	friend bool operator==(const Sales_data&, const Sales_data&);
+	friend bool operator!=(const Sales_data&, const Sales_data&);
+	friend Sales_data operator+(const Sales_data&, const Sales_data&);
+public:
+	Sales_data() = default;
+	Sales_data(const std::string &s) : bookNo(s) {}
+	Sales_data(const std::string &s, unsigned n, double p) :
+		bookNo(s), units_sold(n), revenue(p*n) {}
+	explicit Sales_data(std::istream &is) { is >> *this; }
+	std::string isbn() const { return bookNo; }
+	Sales_data& operator+=(const Sales_data&);
+	Sales_data& operator=(const std::string&);
+private:
+	std::string bookNo;
+	unsigned units_sold = 0;
+	double revenue = 0.0;
+	double avg_price() const;
+};
+
+inline
+Sales_data& Sales_data::operator+=(const Sales_data& rhs)
+{
+	units_sold += rhs.units_sold;
+	revenue += rhs.revenue;
+	return *this;
+}
+inline
+Sales_data& Sales_data::operator=(const std::string &isbn)
+{
+	bookNo = isbn;
+	return *this;
+}
+inline double Sales_data::avg_price() const
+{
+	if (units_sold != 0)
+		return revenue / units_sold;
+	else
+		return revenue;
+}
+inline
+std::ostream& operator<<(std::ostream& os, const Sales_data& item)
+{
+	os << item.bookNo << " "
+		<< item.units_sold << " "
+		<< item.revenue << " "
+		<< item.avg_price();
+	return os;
+}
+inline
+std::istream& operator>>(std::istream &is, Sales_data& item)
+{
+	double price = 0;
+	is >> item.bookNo >> item.units_sold >> price;
+	if (is)
+		item.revenue = item.units_sold * price;
+	else
+		item = Sales_data();
+	return is;
+}
+inline
+bool operator==(const Sales_data &lhs, const Sales_data &rhs)
+{
+	return lhs.bookNo == rhs.bookNo &&
+		lhs.units_sold == rhs.units_sold &&
+		lhs.revenue == rhs.revenue;
+}
+inline
+bool operator!=(const Sales_data &lhs, const Sales_data &rhs)
+{
+	return !(lhs == rhs);
+}
+inline
+Sales_data operator+(const Sales_data &lhs, const Sales_data &rhs)
+{
+	Sales_data sum = lhs;
+	sum += rhs;
+	return sum;
+}
